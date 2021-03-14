ecbe97466895d2659748d32025e0308ab52e384b
Adam Susuł <a.w.susul@gmail.com>
2021-03-14 12:22:19 +0100

    Add next two tasks

--- Basics.swift
+++ Basics.swift
@@ -1,6 +1,5 @@
 import Foundation
 
-
 // Task 1
 // Write a Swift program to compute the sum of the two integers. 
 //If the values are equal return the triple their sum
@@ -45,8 +44,37 @@ func is_string( word: String ) -> String {
 print("Task 3", is_string(word: "Somebody"))
 print("Task 3", is_string(word: "IsSomebody"))
 
+// Task 4
+// Write a Swift program to take the first two characters from a given string 
+// and create a new string with the two characters added at both the front and back
 
+func take_two_characters(word: String) -> String {
 
+    if word.count > 2{
+        let pieceOfWord = word.prefix(2)
+        return pieceOfWord + word + pieceOfWord
+    }
+    return word + word + word
+}
+
+print("Task 4", take_two_characters(word: "D"))
+
+// Task 5 Write a Swift program that return true if either of two given integers
+// is in the range 10..30 inclusive
+
+func value_in_range(firstValue: Int, secondValue: Int) -> Bool {
+
+    if firstValue >= 10 && firstValue<=30{
+        return true
+    }
+    if secondValue >= 10 && secondValue<=30{
+        return true
+    }
+    return false
+
+}
+
+print("Task 5", value_in_range(firstValue: 1, secondValue: 40))
 
 
 
